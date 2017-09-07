ec27a11f39dced265e8dd4c54414e5f5c322958d
Robin Deits <robin.deits@gmail.com>
2017-09-07 11:22:56 -0400

    don't flip sign when normalizing quaternion

--- src/angleaxis_types.jl
+++ src/angleaxis_types.jl
@@ -82,9 +82,10 @@ end
 
 @inline function Base.convert(::Type{AA}, q::Quat) where AA <: AngleAxis
     # TODO: consider how to deal with derivative near theta = 0
+    qs = sign(q.w)
     s = sqrt(q.x*q.x + q.y*q.y + q.z*q.z)
-    theta =  2 * atan2(s, q.w)
-    return s > 0 ? AA(theta, q.x / s, q.y / s, q.z / s) : AA(theta, one(theta), zero(theta), zero(theta))
+    theta =  2 * atan2(s, qs * q.w)
+    return s > 0 ? AA(theta, qs * q.x / s, qs * q.y / s, qs * q.z / s) : AA(theta, one(theta), zero(theta), zero(theta))
 end
 
 # Using Rodrigues formula on an AngleAxis parameterization (assume unit axis length) to do the rotation
